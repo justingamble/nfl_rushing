0541f94af31048d5da19dc506c163ef564ad01db
Justin Gamble <justin.gamble@ualberta.net>
2021-08-15 01:28:25 -0600

    Renamed page_live_test.exs -> player_list_test.exs. Shortened first test. Added broken 3rd test.

--- test/nfl_rushing_web/live/player_live_test.exs
+++ test/nfl_rushing_web/live/player_live_test.exs
@@ -3,162 +3,49 @@ defmodule NflRushingWeb.PlayerLiveTest do
 
   import Phoenix.LiveViewTest
 
-  alias NflRushing.PlayerStats
-
-  @create_attrs %{
-    longest_rush: "some longest_rush",
-    player_name: "some player_name",
-    player_position: "some player_position",
-    rushing_attempts: 42,
-    rushing_attempts_per_game_avg: 120.5,
-    rushing_avg_yards_per_attempt: 120.5,
-    rushing_first_down_percentage: 120.5,
-    rushing_first_downs: 42,
-    rushing_forty_plus_yards_each: 42,
-    rushing_fumbles: 42,
-    rushing_twenty_plus_yards_each: 42,
-    rushing_yards_per_game: 120.5,
-    team_name: "some team_name",
-    total_rushing_touchdowns: 42,
-    total_rushing_yards: 42
-  }
-  @update_attrs %{
-    longest_rush: "some updated longest_rush",
-    player_name: "some updated player_name",
-    player_position: "some updated player_position",
-    rushing_attempts: 43,
-    rushing_attempts_per_game_avg: 456.7,
-    rushing_avg_yards_per_attempt: 456.7,
-    rushing_first_down_percentage: 456.7,
-    rushing_first_downs: 43,
-    rushing_forty_plus_yards_each: 43,
-    rushing_fumbles: 43,
-    rushing_twenty_plus_yards_each: 43,
-    rushing_yards_per_game: 456.7,
-    team_name: "some updated team_name",
-    total_rushing_touchdowns: 43,
-    total_rushing_yards: 43
-  }
-  @invalid_attrs %{
-    longest_rush: nil,
-    player_name: nil,
-    player_position: nil,
-    rushing_attempts: nil,
-    rushing_attempts_per_game_avg: nil,
-    rushing_avg_yards_per_attempt: nil,
-    rushing_first_down_percentage: nil,
-    rushing_first_downs: nil,
-    rushing_forty_plus_yards_each: nil,
-    rushing_fumbles: nil,
-    rushing_twenty_plus_yards_each: nil,
-    rushing_yards_per_game: nil,
-    team_name: nil,
-    total_rushing_touchdowns: nil,
-    total_rushing_yards: nil
-  }
-
-  defp fixture(:player) do
-    {:ok, player} = PlayerStats.create_player(@create_attrs)
-    player
+  test "/ redirects to /players", %{conn: conn} do
+    assert {:error, {:redirect, %{to: "/players"}}} = live(conn, "/")
   end
 
-  defp create_player(_) do
-    player = fixture(:player)
-    %{player: player}
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/players")
+
+    assert disconnected_html =~ "NFL Rushing Statistics"
+    refute render(page_live) =~ "NFL Rushing Statistics"  # in root.html.leex template; not part of liveview output
+
+    assert disconnected_html =~ "Filter players"
+    assert render(page_live) =~ "Filter players"
   end
 
-  describe "Index" do
-    setup [:create_player]
+  test "with no filtering, 326 players are queried", %{conn: conn} do
+    {:ok, view, html} = live(conn, "/players")
 
-    test "lists all players", %{conn: conn, player: player} do
-      {:ok, _index_live, html} = live(conn, Routes.player_index_path(conn, :index))
+    result = view
+             |> element("#playerfilter")
+             |> render_submit(%{player_name: ""})
 
-      assert html =~ "Listing Players"
-      assert html =~ player.longest_rush
-    end
+   assert result =~ "326"
+#   assert true == result
+#    assert view
+#           |> element("form #player-filter")
+#           |> render_click() =~ "326"
 
-    test "saves new player", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, Routes.player_index_path(conn, :index))
-
-      assert index_live |> element("a", "New Player") |> render_click() =~
-               "New Player"
-
-      assert_patch(index_live, Routes.player_index_path(conn, :new))
-
-      assert index_live
-             |> form("#player-form", player: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        index_live
-        |> form("#player-form", player: @create_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.player_index_path(conn, :index))
-
-      assert html =~ "Player created successfully"
-      assert html =~ "some longest_rush"
-    end
-
-    test "updates player in listing", %{conn: conn, player: player} do
-      {:ok, index_live, _html} = live(conn, Routes.player_index_path(conn, :index))
-
-      assert index_live |> element("#player-#{player.id} a", "Edit") |> render_click() =~
-               "Edit Player"
-
-      assert_patch(index_live, Routes.player_index_path(conn, :edit, player))
-
-      assert index_live
-             |> form("#player-form", player: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        index_live
-        |> form("#player-form", player: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.player_index_path(conn, :index))
-
-      assert html =~ "Player updated successfully"
-      assert html =~ "some updated longest_rush"
-    end
-
-    test "deletes player in listing", %{conn: conn, player: player} do
-      {:ok, index_live, _html} = live(conn, Routes.player_index_path(conn, :index))
-
-      assert index_live |> element("#player-#{player.id} a", "Delete") |> render_click()
-      refute has_element?(index_live, "#player-#{player.id}")
-    end
+#    assert render(page_live) =~ "326"
   end
 
-  describe "Show" do
-    setup [:create_player]
+##  test "by default 5 players are listed", %{conn: conn} do
+##    {:ok, page_live, disconnected_html} = live(conn, "/players")
+##
+##    rendered = render(page_live)
+##    assert rendered=~ "Aaron Ripkowski"
+###2	Aaron Rodgers	GB	QB	4.2	67	369	5.5	23.1	4	23	25	37.3	3	0	3
+###3	Adam Humphries	TB	WR	0.3	5	18	3.6	1.2	0	8	0	0.0	0	0	0
+###4	Adam Thielen	MIN	WR	0.1	2	15	7.5	0.9	0	11	1	50.0	0	0	0
+###5	Adrian Peterson
+###6	Akeem Hunt
+##  end
 
-    test "displays player", %{conn: conn, player: player} do
-      {:ok, _show_live, html} = live(conn, Routes.player_show_path(conn, :show, player))
+#    IO.puts("Page list is.... [[#{inspect render(page_live), infinite: true}]]")
+#    IO.puts("Disconnected_html is.... [[#{inspect disconnected_html}]]")
 
-      assert html =~ "Show Player"
-      assert html =~ player.longest_rush
-    end
-
-    test "updates player within modal", %{conn: conn, player: player} do
-      {:ok, show_live, _html} = live(conn, Routes.player_show_path(conn, :show, player))
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Player"
-
-      assert_patch(show_live, Routes.player_show_path(conn, :edit, player))
-
-      assert show_live
-             |> form("#player-form", player: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        show_live
-        |> form("#player-form", player: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.player_show_path(conn, :show, player))
-
-      assert html =~ "Player updated successfully"
-      assert html =~ "some updated longest_rush"
-    end
-  end
 end

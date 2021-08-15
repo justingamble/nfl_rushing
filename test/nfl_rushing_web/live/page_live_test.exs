07877d1e44a4ace4a39a946a0df5da3fc6fe8778
Justin Gamble <justin.gamble@ualberta.net>
2021-08-15 00:41:07 -0600

    page_live_test.exs: added 2 sanity tests, both pass

--- test/nfl_rushing_web/live/page_live_test.exs
+++ test/nfl_rushing_web/live/page_live_test.exs
@@ -3,9 +3,22 @@ defmodule NflRushingWeb.PageLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+  test "/ redirects to /players", %{conn: conn} do
+    {:error, {:redirect, %{to: to_redirect}}} = live(conn, "/")
+    assert to_redirect =~ "/players"
   end
+
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/players")
+
+    assert disconnected_html =~ "NFL Rushing Statistics"
+    refute render(page_live) =~ "NFL Rushing Statistics"  # in root.html.leex template; not part of liveview output
+
+    assert disconnected_html =~ "Filter players"
+    assert render(page_live) =~ "Filter players"
+  end
+
+#    IO.puts("Page list is.... [[#{inspect render(page_live), infinite: true}]]")
+#    IO.puts("Disconnected_html is.... [[#{inspect disconnected_html}]]")
+
 end

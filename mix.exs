6cf5eec0df446ead55a6593ed41af9ca2a6b0ccc
Justin Gamble <justin.gamble@ualberta.net>
2021-08-16 02:07:56 -0600

    bumping elixir version to 1.11.3

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule NflRushing.MixProject do
   def project do
     [
       app: :nfl_rushing,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "1.0.0",
+      elixir: "~> 1.11.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,

fa9d3e01d98120bee8ab162bd241d9593acc6136
Justin Gamble <justin.gamble@ualberta.net>
2021-08-22 02:42:37 -0600

    Resolved a warning by removing quotes around 'coveralls:'

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule NflRushing.MixProject do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test

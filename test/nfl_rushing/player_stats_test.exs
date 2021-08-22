d5b667edee14f6239e2130c5dfd6d3c48389ae42
Justin Gamble <justin.gamble@ualberta.net>
2021-08-22 01:59:57 -0600

    player_stats_test.exs: fixed the example data to pass validations, now tests pass

--- test/nfl_rushing/player_stats_test.exs
+++ test/nfl_rushing/player_stats_test.exs
@@ -7,38 +7,38 @@ defmodule NflRushing.PlayerStatsTest do
     alias NflRushing.PlayerStats.Player
 
     @valid_attrs %{
-      longest_rush: "some longest_rush",
+      longest_rush: "12",
       player_name: "some player_name",
-      player_position: "some player_position",
+      player_position: "ABC",
       rushing_attempts: 42,
       rushing_attempts_per_game_avg: 120.5,
-      rushing_avg_yards_per_attempt: 120.5,
-      rushing_first_down_percentage: 120.5,
+      rushing_avg_yards_per_attempt: 39.5,
+      rushing_first_down_percentage: 89.5,
       rushing_first_downs: 42,
-      rushing_forty_plus_yards_each: 42,
-      rushing_fumbles: 42,
-      rushing_twenty_plus_yards_each: 42,
+      rushing_forty_plus_yards_each: 11,
+      rushing_fumbles: 12,
+      rushing_twenty_plus_yards_each: 14,
       rushing_yards_per_game: 120.5,
-      team_name: "some team_name",
-      total_rushing_touchdowns: 42,
-      total_rushing_yards: 42
+      team_name: "UVW",
+      total_rushing_touchdowns: 15,
+      total_rushing_yards: 92
     }
     @update_attrs %{
-      longest_rush: "some updated longest_rush",
+      longest_rush: "24T",
       player_name: "some updated player_name",
-      player_position: "some updated player_position",
+      player_position: "DEF",
       rushing_attempts: 43,
       rushing_attempts_per_game_avg: 456.7,
-      rushing_avg_yards_per_attempt: 456.7,
-      rushing_first_down_percentage: 456.7,
-      rushing_first_downs: 43,
-      rushing_forty_plus_yards_each: 43,
-      rushing_fumbles: 43,
-      rushing_twenty_plus_yards_each: 43,
+      rushing_avg_yards_per_attempt: 42.7,
+      rushing_first_down_percentage: 77.7,
+      rushing_first_downs: 44,
+      rushing_forty_plus_yards_each: 45,
+      rushing_fumbles: 46,
+      rushing_twenty_plus_yards_each: 47,
       rushing_yards_per_game: 456.7,
-      team_name: "some updated team_name",
-      total_rushing_touchdowns: 43,
-      total_rushing_yards: 43
+      team_name: "XYZ",
+      total_rushing_touchdowns: 49,
+      total_rushing_yards: 50
     }
     @invalid_attrs %{
       longest_rush: nil,
@@ -79,21 +79,21 @@ defmodule NflRushing.PlayerStatsTest do
 
     test "create_player/1 with valid data creates a player" do
       assert {:ok, %Player{} = player} = PlayerStats.create_player(@valid_attrs)
-      assert player.longest_rush == "some longest_rush"
+      assert player.longest_rush == "12"
       assert player.player_name == "some player_name"
-      assert player.player_position == "some player_position"
+      assert player.player_position == "ABC"
       assert player.rushing_attempts == 42
       assert player.rushing_attempts_per_game_avg == 120.5
-      assert player.rushing_avg_yards_per_attempt == 120.5
-      assert player.rushing_first_down_percentage == 120.5
+      assert player.rushing_avg_yards_per_attempt == 39.5
+      assert player.rushing_first_down_percentage == 89.5
       assert player.rushing_first_downs == 42
-      assert player.rushing_forty_plus_yards_each == 42
-      assert player.rushing_fumbles == 42
-      assert player.rushing_twenty_plus_yards_each == 42
+      assert player.rushing_forty_plus_yards_each == 11
+      assert player.rushing_fumbles == 12
+      assert player.rushing_twenty_plus_yards_each == 14
       assert player.rushing_yards_per_game == 120.5
-      assert player.team_name == "some team_name"
-      assert player.total_rushing_touchdowns == 42
-      assert player.total_rushing_yards == 42
+      assert player.team_name == "UVW"
+      assert player.total_rushing_touchdowns == 15
+      assert player.total_rushing_yards == 92
     end
 
     test "create_player/1 with invalid data returns error changeset" do
@@ -103,21 +103,21 @@ defmodule NflRushing.PlayerStatsTest do
     test "update_player/2 with valid data updates the player" do
       player = player_fixture()
       assert {:ok, %Player{} = player} = PlayerStats.update_player(player, @update_attrs)
-      assert player.longest_rush == "some updated longest_rush"
+      assert player.longest_rush == "24T"
       assert player.player_name == "some updated player_name"
-      assert player.player_position == "some updated player_position"
+      assert player.player_position == "DEF"
       assert player.rushing_attempts == 43
       assert player.rushing_attempts_per_game_avg == 456.7
-      assert player.rushing_avg_yards_per_attempt == 456.7
-      assert player.rushing_first_down_percentage == 456.7
-      assert player.rushing_first_downs == 43
-      assert player.rushing_forty_plus_yards_each == 43
-      assert player.rushing_fumbles == 43
-      assert player.rushing_twenty_plus_yards_each == 43
+      assert player.rushing_avg_yards_per_attempt == 42.7
+      assert player.rushing_first_down_percentage == 77.7
+      assert player.rushing_first_downs == 44
+      assert player.rushing_forty_plus_yards_each == 45
+      assert player.rushing_fumbles == 46
+      assert player.rushing_twenty_plus_yards_each == 47
       assert player.rushing_yards_per_game == 456.7
-      assert player.team_name == "some updated team_name"
-      assert player.total_rushing_touchdowns == 43
-      assert player.total_rushing_yards == 43
+      assert player.team_name == "XYZ"
+      assert player.total_rushing_touchdowns == 49
+      assert player.total_rushing_yards == 50
     end
 
     test "update_player/2 with invalid data returns error changeset" do

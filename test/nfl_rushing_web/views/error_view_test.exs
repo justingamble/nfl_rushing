97e89fe113a4954370db4e465abf1bfed4cd8ac6
Justin Gamble <justin.gamble@ualberta.net>
2021-08-23 01:14:05 -0600

    extended error_view_test.exs for our custom 404 test

--- test/nfl_rushing_web/views/error_view_test.exs
+++ test/nfl_rushing_web/views/error_view_test.exs
@@ -3,9 +3,21 @@ defmodule NflRushingWeb.ErrorViewTest do
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
+  use Plug.Test
 
-  test "renders 404.html" do
-    assert render_to_string(NflRushingWeb.ErrorView, "404.html", []) == "Not Found"
+  # Credit to https://www.munich-made.com/2020/03/20200304220507-testing-custom-errorview-in-phoenix/
+  # for the idea to create this setup() method, to setup a 'conn'
+  setup do
+    conn =
+      conn(:get, "/something")
+      |> Plug.Conn.put_private(:phoenix_endpoint, NflRushingWeb.Endpoint)
+
+    {:ok, conn: conn}
+  end
+
+  test "renders 404.html", %{conn: conn} do
+    result = render_to_string(NflRushingWeb.ErrorView, "404.html", conn: conn)
+    assert result =~ ~r/Are you looking for the\s*.*href=\"\/players\".*Players statistics page/i
   end
 
   test "renders 500.html" do

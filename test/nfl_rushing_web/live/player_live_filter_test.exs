8163ee6864d5d916defe4a621a3d4937ef19a3e1
Justin Gamble <justin.gamble@ualberta.net>
2021-08-28 17:20:25 -0600

    Refactored to use 3 describe statements

--- test/nfl_rushing_web/live/player_live_filter_test.exs
+++ test/nfl_rushing_web/live/player_live_filter_test.exs
@@ -5,150 +5,161 @@ defmodule NflRushingWeb.PlayerLiveFilterTest do
   require Integer
 
   import NflRushingWeb.PlayerLiveTestHelper,
-    only: [create_test_player: 1, player_path: 1, player_row: 1, player_index: 2]
+    only: [create_test_player: 1, player_row: 1]
 
   @default_page_size 5
 
-  test "Filtering with no matches will show flash message, and not display any players", %{
-    conn: conn
-  } do
-    players =
-      for player_num <- 1..(@default_page_size + 1) do
-        create_test_player(%{player_name: "Player #{player_num}"})
-      end
+  describe "Test 'Filter Players' button when # of players exceeds what can fit on a pagination page" do
+    setup context = %{conn: conn} do
+      players =
+        for player_num <- 1..(@default_page_size + 1) do
+          create_test_player(%{player_name: "Player #{player_num}"})
+        end
 
-    {:ok, view, _html} = live(conn, "/players")
-    assert has_element?(view, "#number-player-results", "#{@default_page_size + 1}")
+      {:ok, view, _html} = live(conn, "/players")
+      assert has_element?(view, "#number-player-results", "#{@default_page_size + 1}")
 
-    view
-    |> form("#player-filter-form", %{player_name: "Non-existent-player"})
-    |> render_submit()
-
-    assert has_element?(view, "#number-player-results", "0")
-
-    assert render(view) =~ ~r/alert-info.*No players matching.*Non-existent-player/s
-
-    for player <- players do
-      refute has_element?(view, player_row(player))
+      Map.merge(context, %{
+        players: players,
+        live_view: view
+      })
     end
 
-    refute has_element?(view, "#player-table")
-  end
+    test "exception: filtering with no matches will show flash message, and not display any players",
+         %{
+           players: players,
+           live_view: view
+         } do
+      view
+      |> form("#player-filter-form", %{player_name: "Non-existent-player"})
+      |> render_submit()
 
-  test "Filtering by exact player name will display the single player", %{conn: conn} do
-    players =
-      for player_num <- 1..(@default_page_size + 1) do
-        create_test_player(%{player_name: "Player #{player_num}"})
-      end
+      assert has_element?(view, "#number-player-results", "0")
 
-    {:ok, view, _html} = live(conn, "/players")
-    assert has_element?(view, "#number-player-results", "#{@default_page_size + 1}")
+      assert render(view) =~ ~r/alert-info.*No players matching.*Non-existent-player/s
 
-    view
-    |> form("#player-filter-form", %{player_name: "Player 3"})
-    |> render_submit()
-
-    assert has_element?(view, "#number-player-results", "1")
-
-    for player <- players do
-      if player.player_name == "Player 3" do
-        assert has_element?(view, player_row(player))
-      else
+      for player <- players do
         refute has_element?(view, player_row(player))
       end
-    end
-  end
 
-  test "Filtering by substring that matches start of multiple player names (case sensitive)", %{
-    conn: conn
-  } do
-    players =
-      for player_num <- 1..@default_page_size do
-        if Integer.is_even(player_num) do
-          create_test_player(%{player_name: "Matched Player #{player_num}"})
+      refute has_element?(view, "#player-table")
+    end
+
+    test "success: filtering by exact player name will display the single player", %{
+      players: players,
+      live_view: view
+    } do
+      expected_player_name = "Player #{@default_page_size + 1}"
+
+      view
+      |> form("#player-filter-form", %{player_name: expected_player_name})
+      |> render_submit()
+
+      assert has_element?(view, "#number-player-results", "1")
+
+      for player <- players do
+        if player.player_name == expected_player_name do
+          assert has_element?(view, player_row(player))
         else
-          create_test_player(%{player_name: "Excluded Player #{player_num}"})
+          refute has_element?(view, player_row(player))
         end
       end
+    end
+  end
 
-    {:ok, view, _html} = live(conn, "/players")
+  describe "Test 'Filter Players' button when mutiple players are matched (case-sensitive)" do
+    setup context = %{conn: conn} do
+      players =
+        for player_num <- 1..@default_page_size do
+          if Integer.is_even(player_num) do
+            create_test_player(%{player_name: "Matched Player #{player_num}"})
+          else
+            create_test_player(%{player_name: "Excluded Player #{player_num}"})
+          end
+        end
 
-    assert has_element?(view, "#number-player-results", "#{@default_page_size}")
+      {:ok, view, _html} = live(conn, "/players")
+      assert has_element?(view, "#number-player-results", "#{@default_page_size}")
 
-    view
-    |> form("#player-filter-form", %{player_name: "Matched Player"})
-    |> render_submit()
+      Map.merge(context, %{
+        players: players,
+        live_view: view
+      })
+    end
 
-    expected_num_results = Integer.floor_div(@default_page_size, 2)
-    assert has_element?(view, "#number-player-results", "#{expected_num_results}")
+    test "success: filtering by substring that matches start of multiple player names", %{
+      players: players,
+      live_view: view
+    } do
+      view
+      |> form("#player-filter-form", %{player_name: "Matched Player"})
+      |> render_submit()
 
-    for player <- players do
-      if player.player_name =~ ~r/Matched/i do
-        assert has_element?(view, player_row(player))
-      else
-        refute has_element?(view, player_row(player))
+      expected_num_results = Integer.floor_div(@default_page_size, 2)
+      assert has_element?(view, "#number-player-results", "#{expected_num_results}")
+
+      for player <- players do
+        if player.player_name =~ ~r/Matched/i do
+          assert has_element?(view, player_row(player))
+        else
+          refute has_element?(view, player_row(player))
+        end
+      end
+    end
+
+    test "Filtering by substring that matches middle of multipler player names (with spaces surrounding match word)",
+         %{
+           players: players,
+           live_view: view
+         } do
+      view
+      |> form("#player-filter-form", %{player_name: " atched play "})
+      |> render_submit()
+
+      expected_num_results = Integer.floor_div(@default_page_size, 2)
+      assert has_element?(view, "#number-player-results", "#{expected_num_results}")
+
+      for player <- players do
+        if player.player_name =~ ~r/Matched/i do
+          assert has_element?(view, player_row(player))
+        else
+          refute has_element?(view, player_row(player))
+        end
       end
     end
   end
 
-  test "Filtering by substring that matches start of multiple player names (case insensitive)",
-       %{conn: conn} do
-    players =
-      for player_num <- 1..@default_page_size do
-        if Integer.is_even(player_num) do
-          create_test_player(%{player_name: "MaTcHeD Player #{player_num}"})
-        else
-          create_test_player(%{player_name: "Excluded Player #{player_num}"})
+  describe "Test 'Filter Players' button when mutiple players are matched (case-insensitive)" do
+    test "Filtering by substring that matches start of multiple player names (case insensitive)",
+         %{
+           conn: conn
+         } do
+      players =
+        for player_num <- 1..@default_page_size do
+          if Integer.is_even(player_num) do
+            create_test_player(%{player_name: "MaTcHeD Player #{player_num}"})
+          else
+            create_test_player(%{player_name: "Excluded Player #{player_num}"})
+          end
         end
-      end
 
-    {:ok, view, _html} = live(conn, "/players")
+      {:ok, view, _html} = live(conn, "/players")
 
-    assert has_element?(view, "#number-player-results", "#{@default_page_size}")
+      assert has_element?(view, "#number-player-results", "#{@default_page_size}")
 
-    view
-    |> form("#player-filter-form", %{player_name: "matcHED plaYER"})
-    |> render_submit()
+      view
+      |> form("#player-filter-form", %{player_name: "matcHED plaYER"})
+      |> render_submit()
 
-    expected_num_results = Integer.floor_div(@default_page_size, 2)
-    assert has_element?(view, "#number-player-results", "#{expected_num_results}")
+      expected_num_results = Integer.floor_div(@default_page_size, 2)
+      assert has_element?(view, "#number-player-results", "#{expected_num_results}")
 
-    for player <- players do
-      if player.player_name =~ ~r/matched/i do
-        assert has_element?(view, player_row(player))
-      else
-        refute has_element?(view, player_row(player))
-      end
-    end
-  end
-
-  test "Filtering by substring that matches middle of multipler player names (with spaces surrounding match word)",
-       %{conn: conn} do
-    players =
-      for player_num <- 1..@default_page_size do
-        if Integer.is_even(player_num) do
-          create_test_player(%{player_name: "Matched Player #{player_num}"})
+      for player <- players do
+        if player.player_name =~ ~r/matched/i do
+          assert has_element?(view, player_row(player))
         else
-          create_test_player(%{player_name: "Excluded Player #{player_num}"})
+          refute has_element?(view, player_row(player))
         end
-      end
-
-    {:ok, view, _html} = live(conn, "/players")
-
-    assert has_element?(view, "#number-player-results", "#{@default_page_size}")
-
-    view
-    |> form("#player-filter-form", %{player_name: " atched play "})
-    |> render_submit()
-
-    expected_num_results = Integer.floor_div(@default_page_size, 2)
-    assert has_element?(view, "#number-player-results", "#{expected_num_results}")
-
-    for player <- players do
-      if player.player_name =~ ~r/Matched/i do
-        assert has_element?(view, player_row(player))
-      else
-        refute has_element?(view, player_row(player))
       end
     end
   end

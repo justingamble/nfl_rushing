57d6577193226bdf306f06678bfd431c5ccdd009
Justin Gamble <justin.gamble@ualberta.net>
2021-08-28 15:35:41 -0600

    Test refactor: extracted the per page test to a separate file

---
+++ test/nfl_rushing_web/live/player_live_per_page_test.exs
@@ -0,0 +1,33 @@
+defmodule NflRushingWeb.PlayerLivePerPageTest do
+  use NflRushingWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  require Integer
+
+  import NflRushingWeb.PlayerLiveTestHelper,
+    only: [create_test_player: 1, player_path: 1, player_row: 1]
+
+  @default_page_size 5
+
+  test "Change per-page setting of pagination", %{conn: conn} do
+    num_players = @default_page_size * 2
+
+    players =
+      for player_num <- 1..num_players do
+        create_test_player(%{player_name: "Player #{player_num}"})
+      end
+
+    {:ok, view, _html} = live(conn, "/players")
+
+    view
+    |> form("#per-page-dropbox", %{per_page_form: %{per_page: num_players}})
+    |> render_change()
+
+    assert_patched(view, player_path(%{page: 1, per_page: num_players}))
+
+    for player <- players do
+      assert has_element?(view, player_row(player))
+    end
+  end
+
+end
--- test/nfl_rushing_web/live/player_live_test.exs
+++ test/nfl_rushing_web/live/player_live_test.exs
@@ -110,27 +110,6 @@ defmodule NflRushingWeb.PlayerLiveTest do
     assert has_element?(view, "#pagination-left-arrow")
   end
 
-  test "Change per-page setting of pagination", %{conn: conn} do
-    num_players = @default_page_size * 2
-
-    players =
-      for player_num <- 1..num_players do
-        create_test_player(%{player_name: "Player #{player_num}"})
-      end
-
-    {:ok, view, _html} = live(conn, "/players")
-
-    view
-    |> form("#per-page-dropbox", %{per_page_form: %{per_page: num_players}})
-    |> render_change()
-
-    assert_patched(view, player_path(%{page: 1, per_page: num_players}))
-
-    for player <- players do
-      assert has_element?(view, player_row(player))
-    end
-  end
-
   test "insert a single player and see the player listed in query results table", %{conn: conn} do
     player_1 = create_test_player(%{player_name: "Player #1"})
 

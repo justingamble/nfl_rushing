873f43fcbe5fbb91aeaec595835f2423d46762b7
Justin Gamble <justin.gamble@ualberta.net>
2021-08-29 19:25:01 -0600

    Added test to verify the download message is broadcast to pubsub

--- test/nfl_rushing_web/controllers/api/download_controller_test.exs
+++ test/nfl_rushing_web/controllers/api/download_controller_test.exs
@@ -1,70 +1,98 @@
 defmodule NflRushingWeb.Api.DownloadControllerTest do
   use NflRushingWeb.ConnCase
 
+  alias NflRushingWeb.Endpoint
+
   @download_path "/api/download"
   @row_delimiter "\r\n"
   @file_header_row "Player,Team,Pos,Att/G,Att,Yds,Avg,Yds/G,TD,Lng,1st,1st%,20+,40+,FUM#{
                      @row_delimiter
                    }"
+  @download_results_topic "download_results"
 
-  test "success: downloads a single player record and downloaded data is correct", %{conn: conn} do
-    player1 = create_test_player(%{player_name: "Player #1"})
+  describe "Validate the data that is downloaded" do
+    test "success: downloads a single player record and downloaded data is correct", %{conn: conn} do
+      player1 = create_test_player(%{player_name: "Player #1"})
 
-    # Player2 exists in this test just to show that the player filter excludes Player2 results
-    _player2 = create_test_player(%{player_name: "Player #2"})
+      # Player2 exists in this test just to show that the player filter excludes Player2 results
+      _player2 = create_test_player(%{player_name: "Player #2"})
 
-    response =
-      conn
-      |> put_req_header("content-type", "text/csv; charset=utf-8")
-      |> get(@download_path, %{sort_by: :player_name, player_filter: player1.player_name})
+      response =
+        conn
+        |> put_req_header("content-type", "text/csv; charset=utf-8")
+        |> get(@download_path, %{sort_by: :player_name, player_filter: player1.player_name})
 
-    expected_csv = @file_header_row <> get_csv_row_for_player(player1)
+      expected_csv = @file_header_row <> get_csv_row_for_player(player1)
 
-    assert response.resp_body == expected_csv
+      assert response.resp_body == expected_csv
+    end
+
+    test "success: multiple players can be sorted and filtered in download results", %{conn: conn} do
+      player1 = create_test_player(%{player_name: "Player Alfred", total_rushing_touchdowns: 50})
+      player2 = create_test_player(%{player_name: "Player Bob", total_rushing_touchdowns: 40})
+      player3 = create_test_player(%{player_name: "Player Chris", total_rushing_touchdowns: 90})
+      player4 = create_test_player(%{player_name: "Player David", total_rushing_touchdowns: 60})
+
+      response =
+        conn
+        |> put_req_header("content-type", "text/csv; charset=utf-8")
+        |> get(@download_path, %{sort_by: :total_rushing_touchdowns, player_filter: "Player"})
+
+      expected_csv =
+        @file_header_row <>
+          get_csv_row_for_player(player2) <>
+          get_csv_row_for_player(player1) <>
+          get_csv_row_for_player(player4) <>
+          get_csv_row_for_player(player3)
+
+      assert response.resp_body == expected_csv
+    end
   end
 
-  test "success: multiple players can be sorted and filtered in download results", %{conn: conn} do
-    player1 = create_test_player(%{player_name: "Player Alfred", total_rushing_touchdowns: 50})
-    player2 = create_test_player(%{player_name: "Player Bob", total_rushing_touchdowns: 40})
-    player3 = create_test_player(%{player_name: "Player Chris", total_rushing_touchdowns: 90})
-    player4 = create_test_player(%{player_name: "Player David", total_rushing_touchdowns: 60})
+  describe "Invalid download URI provided" do
+    # Expectation comes from:
+    # https://stackoverflow.com/questions/11746894/what-is-the-proper-rest-response-code-for-a-valid-request-but-an-empty-data
+    test "exception: if player filter does not resolve to a player, return status 404", %{
+      conn: conn
+    } do
+      _player1 = create_test_player(%{player_name: "Player Alfred", total_rushing_touchdowns: 50})
+      _player2 = create_test_player(%{player_name: "Player Bob", total_rushing_touchdowns: 40})
 
-    response =
-      conn
-      |> put_req_header("content-type", "text/csv; charset=utf-8")
-      |> get(@download_path, %{sort_by: :total_rushing_touchdowns, player_filter: "Player"})
+      response =
+        conn
+        |> put_req_header("content-type", "text/csv; charset=utf-8")
+        |> get(@download_path, %{
+          sort_by: :total_rushing_touchdowns,
+          player_filter: "No Such Player"
+        })
 
-    expected_csv =
-      @file_header_row <>
-        get_csv_row_for_player(player2) <>
-        get_csv_row_for_player(player1) <>
-        get_csv_row_for_player(player4) <>
-        get_csv_row_for_player(player3)
-
-    assert response.resp_body == expected_csv
+      assert response.status == 404
+      refute response.resp_body
+    end
   end
 
-  # Expectation comes from:
-  # https://stackoverflow.com/questions/11746894/what-is-the-proper-rest-response-code-for-a-valid-request-but-an-empty-data
-  test "exception: if player filter does not resolve to a player, return status 404", %{
-    conn: conn
-  } do
-    _player1 = create_test_player(%{player_name: "Player Alfred", total_rushing_touchdowns: 50})
-    _player2 = create_test_player(%{player_name: "Player Bob", total_rushing_touchdowns: 40})
+  describe "Broadcast messages sent to Phoenix PubSub" do
+    test "success: download behaviour will publish an event to PubSub", %{conn: conn} do
+      _player_1 = create_test_player(%{player_name: "Player #1"})
 
-    response =
+      Endpoint.subscribe(@download_results_topic)
+
       conn
       |> put_req_header("content-type", "text/csv; charset=utf-8")
-      |> get(@download_path, %{
-        sort_by: :total_rushing_touchdowns,
-        player_filter: "No Such Player"
-      })
+      |> get("/api/download/", %{sort_by: :total_rushing_touchdowns, player_filter: "Player"})
 
-    assert response.status == 404
-    refute response.resp_body
+      expected_payload = %Phoenix.Socket.Broadcast{
+        event: "players_downloaded",
+        payload: %{},
+        topic: @download_results_topic
+      }
+
+      assert_receive ^expected_payload, 5_000
+      Endpoint.unsubscribe(@download_results_topic)
+    end
   end
 
-  def get_csv_row_for_player(player) do
+  defp get_csv_row_for_player(player) do
     "#{player.player_name}," <>
       "#{player.team_name}," <>
       "#{player.player_position}," <>
@@ -83,7 +111,7 @@ defmodule NflRushingWeb.Api.DownloadControllerTest do
       @row_delimiter
   end
 
-  def create_test_player(attrs) do
+  defp create_test_player(attrs) do
     {:ok, player} =
       attrs
       |> Enum.into(%{
